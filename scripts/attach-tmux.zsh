18ac4756bb5a2e761cbaa517a8b9f4ee60d5f1cc
Daven Quinn <dev@davenquinn.com>
2020-11-25 11:27:39 -0500

    Added a bunch of scripts, notably a script for attaching to a named Tmux session

---
+++ scripts/attach-tmux.zsh
@@ -0,0 +1,17 @@
+# A script meant to be sourced that attaches to a session
+# - Used in Desk setup
+# - Requires SESSION variable to be set to the proper
+#   name for the session
+
+SESSIONEXISTS=$(tmux list-sessions | grep $SESSION)
+
+# Only create tmux session if it doesn't already exist
+if [ "$SESSIONEXISTS" = "" ]
+then
+    # Start New Session with our name
+    tmux new-session -d -s $SESSION
+fi
+
+# Attach Session, on the Main window
+tmux -CC attach-session -t $SESSION
+
