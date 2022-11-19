6b85868c62fe9740ee33064217405adf6be04a44
Daven Quinn <dev@davenquinn.com>
2022-11-19 10:48:20 -0600

    Some uncommitted updates

--- scripts/attach-tmux.zsh
+++ scripts/attach-tmux.zsh
@@ -2,16 +2,17 @@
 # - Used in Desk setup
 # - Requires SESSION variable to be set to the proper
 #   name for the session
+if [ "$IN_SESSION" != "$SESSION" ]; then
+  SESSIONEXISTS=$(tmux list-sessions | grep $SESSION)
 
-SESSIONEXISTS=$(tmux list-sessions | grep $SESSION)
+  # Only create tmux session if it doesn't already exist
+  if [ "$SESSIONEXISTS" = "" ]
+  then
+      # Start New Session with our name
+      tmux new-session -d -s $SESSION
+  fi
 
-# Only create tmux session if it doesn't already exist
-if [ "$SESSIONEXISTS" = "" ]
-then
-    # Start New Session with our name
-    tmux new-session -d -s $SESSION
+  # Attach Session, on the Main window
+  tmux -CC attach-session -t $SESSION
+  export IN_SESSION="$SESSION"
 fi
-
-# Attach Session, on the Main window
-tmux -CC attach-session -t $SESSION
-
