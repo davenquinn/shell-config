57ff13559fb2bcfdaf6cc9d98a3eb6586710f0f4
Daven Quinn <dev@davenquinn.com>
2023-09-20 23:59:31 -0500

    Updated zsh configurations

--- __legacy.zsh
+++ __legacy.zsh
@@ -37,3 +37,34 @@ export PIPENV_IGNORE_VIRTUALENVS=1
 
 export PATH="$HOME/.poetry/bin:$PATH"
 export PATH=/Users/Daven/.meteor:$PATH
+
+## DESK project management (disabled for now)
+
+# Desk environment setup
+[ -n "$DESK_ENV" ] && source "$DESK_ENV" || true
+
+# Fuzzy project finder
+pr() {
+  # Go directly to project
+  if [ "$#" -eq 1 ]; then
+    desk . "$1"
+    return
+  fi
+  vm=$(desk ls 2> /dev/null | grep --invert-match "{text}" | fzf | grep -o -e '^[A-Za-z0-9_-]\+')
+  desk . "$vm"
+}
+alias prl='desk ls'
+
+function wr() {
+  # Writing projects only
+  # Go directly to project
+  if [ "$#" -eq 1 ]; then
+    desk . "$1"
+    return
+  fi
+  vm=$(desk ls 2> /dev/null | grep "{text}" | fzf | grep -o -e '^[A-Za-z0-9_-]\+')
+  desk . "$vm"
+}
+
+
+
